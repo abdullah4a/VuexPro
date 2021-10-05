5dad2b366a1253a60f1b943cad4a4bdbe5d6b4e4
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-10-05 13:17:00 +0500

    Installation

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-const state = {
-    Users = [
-        { id: 1, name: "rando User", title: "Programme0" }
-    ]
-};
-const mutations = {};
-const actions = {
+const state = {};
+const mutations = {
 
 };
+const actions = {};
 const getters = {};
 export default new Vuex.Store({
     state,
