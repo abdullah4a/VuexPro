b0effe41552041179934291cfda63e91956c9ceb
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-10-05 15:54:50 +0500

    Task Completed

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-const state = {};
+const state = { User: [{ id: 1, Name: "Ali", salary: 10000 }] };
 const mutations = {
-
+    AddUser(state, obj) {
+        state.User.push(obj)
+    }
 };
-const actions = {};
+const actions = {
+    UserAddAct({ commit }, obj) {
+        commit("AddUser", obj)
+    }
+};
 const getters = {};
+
 export default new Vuex.Store({
     state,
     mutations,
     actions,
-    getters
+    getters,
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <div>{{ a }}</div>
+     -->
+    <ul v-for="user in User" :key="user.id">
+      <li>{{ user.id }}</li>
+      <li>{{ user.Name }}</li>
+      <li>{{ user.salary }}</li>
+      <br /><br />
+    </ul>
+    <div>
+      <label for="Uid">ID</label><br />
+      <input type="number" id="Uid" v-model="U.id" /><br />
+      <label for="Name">Name</label><br />
+      <input type="text" id="Name" v-model="U.Name" /><br />
+      <label for="Salary">Salary</label><br />
+      <input type="number" id="Salary" v-model="U.salary" /><br />
+      <button @click="Add">Save</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 // @ is an alias to /src
 export default {
   name: "Home",
+  data() {
+    return { U: { id: 0, Name: "", salary: 0 } };
+  },
+  methods: {
+    ...mapActions(["UserAddAct"]),
+    Add() {
+      this.UserAddAct(this.U);
+    },
+  },
+  computed: {
+    // a(){
+    //   return this.$store.state.a
+    // }
+    // ...mapState({ a: (state) => state.a }),
+    // ...mapState({ a: "a" }),
+    ...mapState(["User"]),
+  },
 };
 </script>
